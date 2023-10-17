785274fd4c9ae6529978b81b2fa84034d1109292
Riveong <setsubi9@gmail.com>
2023-10-17 21:16:43 +0700

    Adding Hero Page

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const HeroSection = () => {
+    return(
+        <section>
+            <h1 className="text-white mb-4 font-extrabold text-4xl lg:text-6xl">Hiya, I'm Riveong</h1>
+            <p className="text-white text-lg lg:text-xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla ullam praesentium voluptates numquam! Consequuntur consequatur maiores commodi impedit, praesentium voluptas quam ullam rerum facere nulla sunt quaerat tempora ipsum perspiciatis.</p>
+        </section>
+    )
+
+
+}
+
+export default HeroSection
